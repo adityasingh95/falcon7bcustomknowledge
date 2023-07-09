d5597b40ecd47036c5ab240ca2b028bd1d4c41f8
Aditya Singh <adityasingh95.game@gmail.com>
2023-07-09 13:56:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,85 @@
+import os
+key = input("Please enter OpenAI API Key")
+os.environ["OPENAI_API_KEY"] =key
 
+EMB_OPENAI_ADA = "text-embedding-ada-002"
+EMB_INSTRUCTOR_XL = "hkunlp/instructor-xl"
+EMB_SBERT_MPNET_BASE = "sentence-transformers/all-mpnet-base-v2" 
+
+LLM_OPENAI_GPT35 = "gpt-3.5-turbo"
+LLM_FLAN_T5_XXL = "google/flan-t5-xxl"
+LLM_FLAN_T5_XL = "google/flan-t5-xl"
+LLM_FASTCHAT_T5_XL = "lmsys/fastchat-t5-3b-v1.0"
+LLM_FLAN_T5_SMALL = "google/flan-t5-small"
+LLM_FLAN_T5_BASE = "google/flan-t5-base"
+LLM_FLAN_T5_LARGE = "google/flan-t5-large"
+LLM_FALCON_SMALL = "tiiuae/falcon-7b-instruct"
+
+from langchain.document_loaders import PDFPlumberLoader
+from langchain.text_splitter import CharacterTextSplitter, TokenTextSplitter
+from transformers import pipeline
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain import HuggingFacePipeline
+from langchain.embeddings import HuggingFaceInstructEmbeddings, HuggingFaceEmbeddings
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+import torch
+from transformers import AutoTokenizer
+import re
+
+config = {"persist_directory":None,
+          "load_in_8bit":False,
+          "embedding" : EMB_SBERT_MPNET_BASE,
+          "llm":LLM_FLAN_T5_BASE,
+          }
+
+def create_sbert_mpnet():
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        return HuggingFaceEmbeddings(model_name=EMB_SBERT_MPNET_BASE, model_kwargs={"device": device})
+
+
+def create_flan_t5_base(load_in_8bit=False):
+        # Wrap it in HF pipeline for use with LangChain
+        model="google/flan-t5-base"
+        tokenizer = AutoTokenizer.from_pretrained(model)
+        return pipeline(
+            task="text2text-generation",
+            model=model,
+            tokenizer = tokenizer,
+            max_new_tokens=100,
+            model_kwargs={"device_map": "auto", "load_in_8bit": load_in_8bit, "max_length": 512, "temperature": 0.}
+        )
+
+def create_falcon_instruct_small(load_in_8bit=False):
+        model = "tiiuae/falcon-7b-instruct"
+
+        tokenizer = AutoTokenizer.from_pretrained(model)
+        hf_pipeline = pipeline(
+                task="text-generation",
+                model = model,
+                tokenizer = tokenizer,
+                trust_remote_code = True,
+                max_new_tokens=100,
+                model_kwargs={
+                    "device_map": "auto", 
+                    "load_in_8bit": load_in_8bit, 
+                    "max_length": 512, 
+                    "temperature": 0.01,
+                    "torch_dtype":torch.bfloat16,
+                    }
+            )
+        return hf_pipeline
+
+if config["embedding"] == EMB_SBERT_MPNET_BASE:
+    embedding = create_sbert_mpnet()
+
+load_in_8bit = config["load_in_8bit"]
+if config["llm"] == LLM_FLAN_T5_BASE:
+    llm = create_flan_t5_base(load_in_8bit=load_in_8bit)
+
+pdf_path = "wiki_data_short.pdf"
+loader = PDFPlumberLoader(pdf_path)
+documents = loader.load()
