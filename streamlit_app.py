8ce6ed07d063690eabd70a36b7ea7ebb27026cae
Aditya Singh <adityasingh95.game@gmail.com>
2023-07-09 14:13:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,17 @@ import os
 key = input("Please enter OpenAI API Key")
 os.environ["OPENAI_API_KEY"] =key
 
+import streamlit as st
+from pdf_qa import PdfQA
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+import time
+import shutil
+
 EMB_OPENAI_ADA = "text-embedding-ada-002"
 EMB_INSTRUCTOR_XL = "hkunlp/instructor-xl"
-EMB_SBERT_MPNET_BASE = "sentence-transformers/all-mpnet-base-v2" 
+EMB_SBERT_MPNET_BASE = "sentence-transformers/all-mpnet-base-v2" # Chroma takes care if embeddings are None
+EMB_SBERT_MINILM = "sentence-transformers/all-MiniLM-L6-v2" # Chroma takes care if embeddings are None
 
 LLM_OPENAI_GPT35 = "gpt-3.5-turbo"
 LLM_FLAN_T5_XXL = "google/flan-t5-xxl"
@@ -15,71 +23,15 @@ LLM_FLAN_T5_BASE = "google/flan-t5-base"
 LLM_FLAN_T5_LARGE = "google/flan-t5-large"
 LLM_FALCON_SMALL = "tiiuae/falcon-7b-instruct"
 
-from langchain.document_loaders import PDFPlumberLoader
-from langchain.text_splitter import CharacterTextSplitter, TokenTextSplitter
-from transformers import pipeline
-from langchain.prompts import PromptTemplate
-from langchain.chat_models import ChatOpenAI
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
-from langchain import HuggingFacePipeline
-from langchain.embeddings import HuggingFaceInstructEmbeddings, HuggingFaceEmbeddings
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.llms import OpenAI
-import torch
-from transformers import AutoTokenizer
-import re
-
-config = {"persist_directory":None,
-          "load_in_8bit":False,
-          "embedding" : EMB_SBERT_MPNET_BASE,
-          "llm":LLM_FLAN_T5_BASE,
-          }
-
-def create_sbert_mpnet():
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        return HuggingFaceEmbeddings(model_name=EMB_SBERT_MPNET_BASE, model_kwargs={"device": device})
+# Streamlit app code
+st.set_page_config(
+    page_title='Q&A Bot for PDF',
+    page_icon='🔖',
+    layout='wide',
+    initial_sidebar_state='auto',
+)
 
 
-def create_flan_t5_base(load_in_8bit=False):
-        # Wrap it in HF pipeline for use with LangChain
-        model="google/flan-t5-base"
-        tokenizer = AutoTokenizer.from_pretrained(model)
-        return pipeline(
-            task="text2text-generation",
-            model=model,
-            tokenizer = tokenizer,
-            max_new_tokens=100,
-            model_kwargs={"device_map": "auto", "load_in_8bit": load_in_8bit, "max_length": 512, "temperature": 0.}
-        )
+if "pdf_qa_model" not in st.session_state:
+    st.session_state["pdf_qa_model"]:PdfQA = PdfQA() ## Intialisation
 
-def create_falcon_instruct_small(load_in_8bit=False):
-        model = "tiiuae/falcon-7b-instruct"
-
-        tokenizer = AutoTokenizer.from_pretrained(model)
-        hf_pipeline = pipeline(
-                task="text-generation",
-                model = model,
-                tokenizer = tokenizer,
-                trust_remote_code = True,
-                max_new_tokens=100,
-                model_kwargs={
-                    "device_map": "auto", 
-                    "load_in_8bit": load_in_8bit, 
-                    "max_length": 512, 
-                    "temperature": 0.01,
-                    "torch_dtype":torch.bfloat16,
-                    }
-            )
-        return hf_pipeline
-
-if config["embedding"] == EMB_SBERT_MPNET_BASE:
-    embedding = create_sbert_mpnet()
-
-load_in_8bit = config["load_in_8bit"]
-if config["llm"] == LLM_FLAN_T5_BASE:
-    llm = create_flan_t5_base(load_in_8bit=load_in_8bit)
-
-pdf_path = "wiki_data_short.pdf"
-loader = PDFPlumberLoader(pdf_path)
-documents = loader.load()
